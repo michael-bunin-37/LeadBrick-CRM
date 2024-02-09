43c46f10334e399da399a5dbf06e6f61381bbfd4
miamorekun <buninmisha1809@gmail.com>
2024-02-09 08:55:35 +0200

    Fix: Projects Delete

--- src/features/project/project-delete/model.tsx
+++ src/features/project/project-delete/model.tsx
@@ -1,17 +1,24 @@
 import {api} from "@/utils/api/api"
 import {API_URL} from "@/utils/config"
 import {QueryKeys} from "@/utils/query-keys"
-import {UseMutationOptions, useMutation, useQueryClient} from "@tanstack/react-query"
+import {
+	UseMutationOptions,
+	useMutation,
+	useQueryClient,
+} from "@tanstack/react-query"
 import {useRouter} from "next/router"
 
-export const useProjectDelete = (config?: UseMutationOptions<string, Error, {chatId: string}>) => {
+export const useProjectDelete = (
+	config?: UseMutationOptions<string, Error, {chatId: string}>,
+) => {
 	const queryClient = useQueryClient()
 	const {push} = useRouter()
 	return useMutation({
-		mutationFn: (vars) => api.delete(`${API_URL}/project/tag/${vars.chatId}`).text(),
+		mutationFn: (vars) =>
+			api.delete(`${API_URL}/project/${vars.chatId}`).text(),
 		onSuccess(data, variables, context) {
 			queryClient.invalidateQueries({queryKey: [QueryKeys["PROJECT"]]})
-			push("/projects")
+			push("/app/projects")
 		},
 		...config,
 	})
